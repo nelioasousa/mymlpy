5142caa8f2b8d60b37bddac4e1dd69030fced3e2
nelioasousa <corneliosousaphb@gmail.com>
2024-12-05 21:26:06 -0300

    Data partition functionality

--- src/mymlpy/datasets/__init__.py
+++ src/mymlpy/datasets/__init__.py
@@ -1,3 +1,5 @@
 from mymlpy.datasets._tabular_iterator import TabularDatasetBatchIterator
+from mymlpy.datasets._split import KFold
+from mymlpy.datasets._split import split_data
 
-__all__ = [TabularDatasetBatchIterator.__name__]
+__all__ = [TabularDatasetBatchIterator.__name__, KFold.__name__, split_data.__name__]
---
+++ src/mymlpy/datasets/_split.py
@@ -0,0 +1,19 @@
+def split_data(data_array, proportions, shuffle=True, categorizer=None):
+    return
+
+
+class KFold:
+    def __init__(self, data_array, k, shuffle=True, categorizer=None):
+        return
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return
+
+    def get_fold(self, fold_index):
+        return
+
+    def refold(self, shuffle=True):
+        return
