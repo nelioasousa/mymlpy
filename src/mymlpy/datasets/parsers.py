c0d299e151d24f35fcda59c2636a84d66a1a0418
nelioasousa <corneliosousaphb@gmail.com>
2024-12-10 17:18:07 -0300

    Categorical parsers impl & unit tests

--- src/mymlpy/datasets/parsers.py
+++ src/mymlpy/datasets/parsers.py
@@ -29,3 +29,85 @@ def missing_data(missing_data_repr, missing_data_placeholder=None, case_sensitiv
         return wrapper
 
     return parser_decorator
+
+
+def _unique_entries(data, sort_key):
+    try:
+        data = data.flatten()
+    except AttributeError:
+        pass
+    categories = list(frozenset(data))
+    categories.sort(key=sort_key)
+    return categories
+
+
+class OneHotParser:
+    def __init__(
+        self, categories, ignore_unknowns=False, case_sensitive=True, strip_values=False
+    ):
+        categories = categories if case_sensitive else (c.casefold() for c in categories)
+        categories = (c.strip() for c in categories) if strip_values else categories
+        self.categories = tuple(categories)
+        if len(self.categories) != len(frozenset(self.categories)):
+            raise ValueError("`categories` must contain unique entries.")
+        self.ignore_unknowns = ignore_unknowns
+        self.case_sensitive = case_sensitive
+        self.strip_values = strip_values
+
+    def __call__(self, value):
+        value = value if self.case_sensitive else value.casefold()
+        value = value.strip() if self.strip_values else value
+        onehot = [value == category for category in self.categories]
+        if not self.ignore_unknowns and not sum(onehot):
+            raise ValueError(f"'{value}' doesn't match any category.")
+        return onehot
+
+    @classmethod
+    def from_data(
+        cls,
+        data,
+        sort_key=None,
+        ignore_unknowns=False,
+        case_sensitive=True,
+        strip_values=False,
+    ):
+        categories = _unique_entries(data, sort_key)
+        return cls(categories, ignore_unknowns, case_sensitive, strip_values)
+
+
+class IndexParser(OneHotParser):
+    def __init__(
+        self, categories, unknowns_index=None, case_sensitive=True, strip_values=False
+    ):
+        if unknowns_index is None:
+            ignore_unknowns = False
+        elif isinstance(unknowns_index, int):
+            ignore_unknowns = True
+            unknowns_index = int(unknowns_index)
+        else:
+            raise ValueError("`unknowns_index` must be either `None` or an `int`.")
+        super().__init__(
+            categories=categories,
+            ignore_unknowns=ignore_unknowns,
+            case_sensitive=case_sensitive,
+            strip_values=strip_values,
+        )
+        self.unknowns_index = unknowns_index
+
+    def __call__(self, value):
+        onehot = super().__call__(value)
+        if not sum(onehot):
+            return self.unknowns_index
+        return next(i for i, value in enumerate(onehot) if value)
+
+    @classmethod
+    def from_data(
+        cls,
+        data,
+        sort_key=None,
+        unknowns_index=None,
+        case_sensitive=True,
+        strip_values=False,
+    ):
+        categories = _unique_entries(data, sort_key)
+        return cls(categories, unknowns_index, case_sensitive, strip_values)
--- tests/datasets/parsers_test.py
+++ tests/datasets/parsers_test.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 
 from mymlpy.datasets import parsers
 
@@ -35,3 +36,83 @@ def test_missing_data(missing_repr, case_sensitive, missing_data_values, data_va
         assert dummy_parser(value) is None, "Failed to parse missing data '%s'" % value
     for value in data_values:
         assert dummy_parser(value) is value, "Failed to parse data '%s'" % value
+
+
+def test_onehot_parser():
+    categories = ("male", "female", "other")
+    parser = parsers.OneHotParser(categories)
+    for category in categories:
+        target = [c == category for c in categories]
+        assert parser(category) == target
+
+
+def test_onehot_parser_ignore_unknowns():
+    categories = ("awful", "bad", "average", "good", "perfect")
+    parser = parsers.OneHotParser(categories, ignore_unknowns=True)
+    target = [False] * len(categories)
+    unknowns = ("horrible", "nice", "ordinary")
+    for unknown in unknowns:
+        assert parser(unknown) == target
+    parser.ignore_unknowns = False
+    for unknown in unknowns:
+        with pytest.raises(ValueError):
+            parser(unknown)
+
+
+def test_onehot_parser_case_insensitive():
+    categories = ("N", "S")
+    parser = parsers.OneHotParser(categories, case_sensitive=False)
+    for category in categories + tuple(c.casefold() for c in categories):
+        folded_category = category.casefold()
+        target = [folded_category == c.casefold() for c in categories]
+        assert parser(category) == target
+
+
+def test_onehot_parser_strip_values():
+    categories = (" empty", " half ", " full")
+    parser = parsers.OneHotParser(categories, strip_values=True)
+    for category in ("empty", "empty ", "half", "full "):
+        split_category = category.strip()
+        target = [split_category == c.strip() for c in categories]
+        assert parser(category) == target
+
+
+def test_onehot_parser_case_strip():
+    categories = (" eMpTy", " haLF ", " full")
+    parser = parsers.OneHotParser(categories, case_sensitive=False, strip_values=True)
+    processed_categories = tuple(c.strip().casefold() for c in categories)
+    for category in ("empty", "EmPtY ", "  HAlf", "   fULL "):
+        processed_category = category.strip().casefold()
+        target = [processed_category == c for c in processed_categories]
+        assert parser(category) == target
+
+
+def test_onehot_parser_from_data():
+    categories = ("missing", "found", "destroyed")
+    data = np.random.randint(0, len(categories), 10)
+    data = np.array([categories[idx] for idx in data])
+    sort_key = lambda x: categories.index(x)
+    parser = parsers.OneHotParser.from_data(data, sort_key=sort_key)
+    for category in categories:
+        target = [c == category for c in categories]
+        assert parser(category) == target
+
+
+def test_index_parser():
+    categories = ("S", "M", "L", "XL")
+    parser = parsers.IndexParser(categories)
+    for i, category in enumerate(categories):
+        assert parser(category) == i
+
+
+def test_index_parser_ignore_unknowns():
+    categories = ("go", "slow", "stop")
+    unknowns_index = -1
+    parser = parsers.IndexParser(categories, unknowns_index=unknowns_index)
+    unknowns = ("halt", "wait", "fast")
+    for unknown in unknowns:
+        assert parser(unknown) == unknowns_index
+    parser.ignore_unknowns = False
+    for unknown in unknowns:
+        with pytest.raises(ValueError):
+            parser(unknown)
