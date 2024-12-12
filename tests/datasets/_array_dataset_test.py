1690852f1fe9b7f94e6aea1dbbd56c9e137e60ee
nelioasousa <corneliosousaphb@gmail.com>
2024-12-12 20:22:41 -0300

    ArrayDataset unit tests

---
+++ tests/datasets/_array_dataset_test.py
@@ -0,0 +1,33 @@
+import pytest
+import numpy as np
+
+from mymlpy.datasets import ArrayDataset
+
+
+@pytest.fixture
+def random_array():
+    size = (np.random.randint(0, 201), np.random.randint(2, 5))
+    info_int8 = np.iinfo(np.int8)
+    return np.random.randint(info_int8.min, info_int8.max + 1, size=size, dtype=np.int8)
+
+
+def test_array_dataset_no_copy(random_array):
+    ds = ArrayDataset(random_array, copy=False)
+    assert ds.get_data_array(copy=False) is random_array
+
+
+def test_array_dataset_copy(random_array):
+    ds = ArrayDataset(random_array, copy=True)
+    ds_array = ds.get_data_array(copy=False)
+    assert ds_array is not random_array
+    assert np.array_equal(ds_array, random_array)
+    ds_array[0, 0] = random_array[0, 0] + 1
+    assert not np.array_equal(ds_array[0], random_array[0])
+
+
+def test_array_dataset_batch_iter(random_array):
+    ds = ArrayDataset(random_array)
+    ds_array_copy = ds.get_data_array(copy=True)
+    for idx, entry in enumerate(ds.batch_iter(1)):
+        np.array_equal(entry[0], random_array[idx])
+    assert not np.array_equal(ds_array_copy, random_array)
