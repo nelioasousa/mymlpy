78fb0fbeb4c796dbc3b11fece774e623ef3e9909
nelioasousa <corneliosousaphb@gmail.com>
2024-12-22 11:42:30 -0300

    LinearRegression impl

--- src/mymlpy/linear/regression.py
+++ src/mymlpy/linear/regression.py
@@ -4,26 +4,22 @@ import numpy as np
 class LinearRegression:
     def __init__(self, ridge_alpha=0.0):
         # Start public
-        self.alpha = ridge_alpha
+        self.ridge_alpha = ridge_alpha
         # End public
-        self._coefficients = None
         self._intercept = None
+        self._coefficients = None
+        self._parameters = None
 
     @property
-    def alpha(self):
-        return self._alpha
+    def ridge_alpha(self):
+        return self._ridge_alpha
 
-    @alpha.setter
-    def alpha(self, value):
-        self._alpha = float(value)
-
-    @property
-    def coefficients(self):
-        return self._coefficients
-
-    @coefficients.setter
-    def coefficients(self, value):
-        raise AttributeError("Read-only attribute.")
+    @ridge_alpha.setter
+    def ridge_alpha(self, value):
+        alpha = float(value)
+        if alpha < 0.0:
+            raise ValueError("`ridge_alpha` must be non-negative.")
+        self._ridge_alpha = alpha
 
     @property
     def intercept(self):
@@ -33,11 +29,94 @@ class LinearRegression:
     def intercept(self, value):
         raise AttributeError("Read-only attribute.")
 
-    def fit(self, X, y, sample_weights=None):
+    @property
+    def coefficients(self):
+        if self._coefficients is None:
+            return None
+        return np.copy(self._coefficients)
+
+    @coefficients.setter
+    def coefficients(self, value):
+        raise AttributeError("Read-only attribute.")
+
+    @property
+    def parameters(self):
+        if self._parameters is None:
+            return None
+        return np.copy(self._parameters)
+
+    @parameters.setter
+    def parameters(self, value):
+        raise AttributeError("Read-only attribute.")
+
+    def _check_X(self, X, features_dim=None):
+        if not X.shape[0]:
+            raise ValueError("`X` is empty.")
+        if len(X.shape) != 2:
+            raise ValueError("`X` must be a 2D-array (a matrix).")
+        if features_dim is not None and X.shape[1] != features_dim:
+            raise ValueError(
+                f"Expecting `X` to have observations of size {features_dim} (axis 1 size)."
+            )
         return
 
+    def _check_y(self, y, N):
+        if len(y.shape) > 3:
+            raise ValueError("`y` must be a flat array or a column vector.")
+        if len(y.shape) == 2 and y.shape[1] != 1:
+            raise ValueError("Multi-output regression not supported.")
+        try:
+            return y.reshape((N, 1))
+        except ValueError:
+            raise ValueError("`y` isn't consistent with `X`.")
+
+    def _check_sample_weights(self, sample_weights, N):
+        if len(sample_weights) > 3:
+            raise ValueError("`sample_weights` must be a flat array or a column vector.")
+        try:
+            sample_weights = sample_weights.reshape((N, 1))
+        except ValueError:
+            raise ValueError("`sample_weights` isn't consistent with `X`.")
+        sample_weights[:] = sample_weights / sample_weights.sum()
+        return sample_weights
+
+    def fit(self, X, y, sample_weights=None):
+        X = np.asarray(X)
+        self._check_X(X)
+        N, P = X.shape
+        y = self._check_y(np.asarray(y, dtype=X.dtype), N)
+        X_ext = np.concatenate((np.ones((N, 1), dtype=X.dtype), X), axis=1)
+        if sample_weights is None:
+            X_t = X_ext.transpose()
+        else:
+            sample_weights = self._check_sample_weights(
+                np.asarray(sample_weights, dtype=X.dtype), N
+            )
+            X_t = (sample_weights * X_ext).transpose()
+        if self.ridge_alpha > 0.0:
+            l2_reg = self.ridge_alpha * np.identity(P + 1, dtype=X.dtype)
+            l2_reg[0, 0] = 0  # Do not regularize intercept
+            inv = np.linalg.inv((X_t @ X) + l2_reg)
+        else:
+            inv = np.linalg.inv(X_t @ X)
+        params = inv @ X_t @ y
+        self._intercept = params[0, 0]
+        self._coefficients = params[:, 0][1:]
+        self._parameters = params
+        return self.parameters
+
     def predict(self, X):
-        return
+        coefficients = self._coefficients
+        if coefficients is None:
+            raise RuntimeError("Model not fitted do any data.")
+        X = np.asarray(X, dtype=coefficients.dtype)
+        if len(X.shape) == 1:
+            try:
+                return X * coefficients + self._intercept
+            except ValueError:
+                raise ValueError("`X` has an inconsistent dimension.")
+        self._check_X(X, len(coefficients))
+        return X @ coefficients + self._intercept
 
 
 class StochasticLinearRegression(LinearRegression):
