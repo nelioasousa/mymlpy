4b99d92c2fe7e0b54c94621b624e036d92d2292f
nelioasousa <corneliosousaphb@gmail.com>
2025-01-06 14:24:26 -0300

    Improve documentation pt.4

--- src/mymlpy/activations.py
+++ src/mymlpy/activations.py
@@ -4,5 +4,19 @@ import numpy as np
 
 
 def sigmoid(z):
+    """Computes the sigmoid function.
+
+    The sigmoid function is defined as 1 / (1 + exp(-x)).
+
+    Parameters:
+        `z` (`Union[numbers.Real, numpy.typing.ArrayLike]`) - Input value(s).
+
+    Returns:
+        `Union[numbers.Real, numpy.typing.ArrayLike]` - Sigmoid of the input value(s).
+
+    Raises:
+
+        No exception is directly raised.
+    """
     z = np.asarray(z)
     return 1 / (1 + np.exp(-z))
